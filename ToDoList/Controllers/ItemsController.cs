2dd937d4244390a5665bfa6415f0e7877df15e60
Brooke Kullberg <brookekullberg@gmail.com>
2022-11-16 09:54:42 -0800

    add workin project

---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    private readonly ToDoListContext _db;
+
+    public ItemsController(ToDoListContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Item> model = _db.Items.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Item item)
+    {
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+  }
+}
---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,8 @@
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public int ItemId { get; set; }
+    public string Description { get; set; }
+  }
+}
